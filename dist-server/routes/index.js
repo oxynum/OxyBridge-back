b887d6c2ad3c7c7857386f803a6b7c8db2dfdeb6
AnneLaureSIEVERING <77326841+AnneLaureSIEVERING@users.noreply.github.com>
2021-01-21 17:53:30 +0100

    [project-setup]init node, create start scripts, stable state (#3)
    Co-authored-by: AnneLaureSIEVERING <anne-laure@oxynum.fr>

---
+++ dist-server/routes/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// routes/index.js and users.js
+var router = _express["default"].Router();
+/* GET home page. */
+
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+}); // module.exports = router;
+// ..stuff below
+
+var _default = router;
+exports["default"] = _default;
